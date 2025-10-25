294fc586d55021003e914fc0e0e8477a8ea2a666
Joao Ferreira <yferreirajoao@gmail.com>
2025-10-25 15:41:30 -0300

    Adicionando 3 frases no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function Home() {
-    return <h1>Hello World!</h1>
+    return (
+        <div className="home-container">
+            <h1>Hello World!</h1>
+            <p>This is my first React app.</p>
+
+            <br></br>
+
+            <h2>Frase da Asuna</h2>
+            <p>"Eu oudeio Fisica e Analise Matematica 2" </p>
+
+            <br></br>
+
+            <h2>Frase do Ygor</h2>
+            <p>"O Futuro é Pica"C</p>
+
+            <br></br>
+
+            <h2>Frase do Haisuu</h2>
+            <p>"Tô tendo que aprender React..."</p>
+
+        </div>
+    )
 }
 
 export default Home;
